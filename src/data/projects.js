5de97103437b3a2762f875b05c14fcb0e2aa16d7
Raphael Piechocki <raph215215@gmail.com>
2026-03-23 21:07:42 +0100

    feat: add new images for 'Dev Simulator' project and update image references in `projects.js`

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,9 +7,9 @@ export const projectsData = [
     fullDescription: `Projetez-vous dans la peau d'un développeur web avec "Dev Simulator", un jeu de clicker idle où vous commencez en tant que stagiaire et évoluez jusqu'à devenir un développeur senior. Gagnez des points de compétence en cliquant, débloquez de nouvelles technologies, gérez votre temps et votre énergie pour progresser dans votre carrière virtuelle. Un mélange addictif de stratégie et de progression pour les passionnés de développement.`,
 
     image: '/images/mockup_dev-simulator.webp',
-    imageHero: '/videos/pub_devsimulator.mp4',
+    imageHero: '/images/dev_simulator2.png',
     imageDescription: '/images/accueil_dev-simulator.png',
-    imageTechnologies: '/videos/pub_devsimulator2.mp4',
+    imageTechnologies: '/images/dev_simulator2.png',
 
     gallery: [
       
